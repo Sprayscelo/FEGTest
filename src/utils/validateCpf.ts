bcd7a36dd6ea710b016154af3a015c5266429905
sprayscelo <marcelo_marchioro@hotmail.com>
2024-01-22 01:30:43 -0300

    fix: fixed  function to also validate CNPJ

--- src/utils/validateCpf.ts
+++ src/utils/validateCpf.ts
@@ -2,7 +2,7 @@ export default function formatCPF(cpf: string): boolean {
   if (cpf.length !== 11) {
     return false;
   }
-  const regExpCpf = /^\d{3}\d{3}\d{3}\d{2}$/;
+  const regExpCpf = /^(\d{11}|\d{14})$/;
   const isValidCpf = regExpCpf.test(cpf);
 
   return isValidCpf;
